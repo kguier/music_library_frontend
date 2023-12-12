0dbbd48c3b09c1aa07dd68b03f75d8e44ceb33a9
Kira <knguier12@gmail.com>
2023-12-11 21:00:47 -0500

    Axios used to consume Backend API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import axios from "axios";
 
 function App() {
+  const fetchMusic = async () => {
+    try {
+      const response = await axios.get("https://localhost:7149/api/Songs");
+      console.log(response);
+    } catch (error) {
+      console.warn("Error in fetchMusic request: ", error);
+    }
+  };
+
   return (
     <div className="App">
       <Header />
+      <button onClick={fetchMusic}>Fetch Music</button>
     </div>
   );
 }
